42ffd4d3b24b4fadd495a0f20bc310683b9681b0
shhipahi <rojan.shipai2064@gmail.com>
2026-02-15 23:53:50 +0545

    testing

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
